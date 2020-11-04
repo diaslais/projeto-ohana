b9f559b8e7f86a57cf35696cc1a3e767f4ed2082
Arthur Castilho <castartx@gmail.com>
2020-11-04 18:01:26 -0300

    Quem-somos 1.0
    Falta correcao da posicao do footer, posicao dos titulos e imagens do "nossa equipe"

---
+++ quem-somos.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/join.css">
+    
+    <title>Quem Somos</title>
+</head>
+
+<body>
+     <?php include'header.html';?> 
+
+    <main class="quem-somos-main">
+        <section class="section-quem-somos">
+            <div id="img-quem-somos">
+                <!--<img src="voluntario.png" alt="quem-somos" >-->
+            </div>
+            <div id="texto-quem-somos">
+               QUEM SOMOS
+                <p class="p-quem-somos">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+        </section> 
+
+        <section class="section-missao-historia">
+            <div id="texto-historia">
+                Historia
+                <p class="p-quem-somos">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <div id="texto-missao">
+                Missao
+                <p class="p-quem-somos">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+        </section>
+           
+        <section class="section-equipe">
+            
+            <div class="texto-nossa-equipe">
+                NOSSA EQUIPE
+                <p class="p-quem-somos">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        </section>
+
+        <section class="section-odsatuamos">
+            <div id="rectangle-4">
+
+            </div>
+
+            <div id="rectangle-3">
+
+            </div>
+
+            <div id="rectangle-2">
+
+            </div>
+
+            <div id="texto-odsatuamos">
+                ODS QUE ATUAMOS
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+
+        </section>
+    </main>
+
+
+     <!--<?php include'footer.html';?> -->
+</body>
+</html>
