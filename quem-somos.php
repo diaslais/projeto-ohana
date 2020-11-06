fa69201b66bb83b4ebacc19df0c46a221803965d
Arthur Castilho <castartx@gmail.com>
2020-11-06 11:21:34 -0300

    .

--- quem-somos.php
+++ quem-somos.php
@@ -31,7 +31,7 @@
 
         <section class="section-missao-historia">
             <div id="texto-historia">
-                Historia
+                Historia 
                 <p class="p-quem-somos">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 </p>
