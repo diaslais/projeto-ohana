c3a1f454908a64a0b34a9623ffe3247e517b2d10
Nathan Carvalho <nathan_santos@usp.br>
2020-11-14 10:46:38 -0300

    Alteração no layout

--- ciclo-de-formacao.php
+++ ciclo-de-formacao.php
@@ -6,42 +6,64 @@
     <!-- inicio do corpo da pagina  -->
 
     
+    <div class="formacao-headline">
+        <div class="headline-itens">
+            <h1>Ciclo de Formação</h1>
+        
+            
+            <p>O Ciclo de formações se consiste em um conjunto de treinamentos voltados para gestão
+                de internas de ONGs e OSCIPs neles abordamos assuntos como: marketing para redes
+                sociais, gestão de pessoas, controle financeiro, arrecadação de recursos e etc.
+            </p>
+        </div>
+    </div>
     
     
 
     <div class="page-content">
-        <h1>Ciclo de Formação</h1>
-
-        <div class="descricao-container formacao">
-            <p>O Ciclo de formações se consiste em um conjunto de treinamentos voltados para gestão
-            de internas de ONGs e OSCIPs neles abordamos assuntos como: marketing para redes
-            sociais, gestão de pessoas, controle financeiro, arrecadação de recursos e etc.
-            </p>
-            
-        </div>
-
         
         <div class="participantes-container">
-
             <h2>Organizações que já participaram</h2>
             <div class="lista-img-container">
                 <div class="participantes-lista">
-                    <ul>
-                        
-                        <li>
-                            <a href="https://www.youtube.com/" target="_blank">ong 1:</a>
-                            <p>breve Descrição</p>
-                        </li>
-                        <li>
-                            <a href="#">ong 2:</a>
-                            <p>breve Descrição</p>
-                        </li>
-                        <li>
-                            <a href="#">ong 3:</a>
-                            <p>breve Descrição</p>
-                        </li>
-                        
-                    </ul>
+                    <div class="list-block">
+                        <div class="list-logo">
+                            <a href="#" target="_blank">
+                                <img src="assets/logo_completo.png" alt="Logo da instituição Projeto Ohana">
+                            </a>
+                            <a href="#" target="_blank">
+                                <h3>Ohana</h3>
+                            </a>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, ab!</p>
+                    </div>
+                    <div class="list-block">
+                        <div class="list-logo">
+                            <a href="#" target="_blank">
+                                <img src="assets/logo_completo.png" alt="Logo da instituição Projeto Ohana">
+                            </a>
+                            <a href="#" target="_blank">
+                                <h3>Ohana</h3>
+                            </a>
+                        </div>
+                    
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, assumenda?</p>
+                    </div>
+                    <div class="list-block">
+                        <div class="list-logo">
+                            <a href="#" target="_blank">
+                                <img src="assets/logo_completo.png" alt="Logo da instituição Projeto Ohana">
+                            </a>
+                            <a href="#" target="_blank">
+                                <h3>Ohana</h3>
+                            </a>
+                        </div>
+                    
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, ea.</p>
+                    </div>
+                    
+
                 </div>
 
                 <div class="formacao-imagens">
