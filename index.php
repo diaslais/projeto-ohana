ffeac3b4483f0978827fe5bc23c8b92c9e86099b
Lais Dias <laisdias099@gmail.com>
2020-11-27 16:39:21 -0300

    adicionando texto no impacto

--- index.php
+++ index.php
@@ -6,13 +6,12 @@
         <div class="index-headline-content">
             <div class="index-headline-text">
                 <h1>Quem cuida dos cuidadores?</h1>
-                <p>A partir desse questionamento, buscamos contribuir com ONGs e OSCIPs por meio da educação 
+                <p>A partir desse questionamento, buscamos contribuir com ONGs e OSCIPs por meio da educação
                     e tecnologia a fim de maximizar o impacto de suas atuações.
                 </p>
                 <a href="#index-oferecemos"><button class="index-btn-conheca">Conheça nosso trabalho</button></a>
             </div>
         </div>
-
         <div id="index-oferecemos" class="index-container-oferecemos">
             <h2>O que oferecemos</h2>
             <div class="index-nossas-frentes">
@@ -31,7 +30,7 @@
                     </a>
                 </div>
                 <div class="index-frente-ciclo">
-                    <a href="ciclo-de-formacao.php">
+                    <a href="#">
                         <img src="./assets/ciclo-ilustracao.svg" alt="Ciclo de formação">
                         <h3>Ciclo de formação</h3>
                         <p>Conjunto de treinamentos</p>
@@ -40,24 +39,33 @@
             </div>
             <div class="index-pesquisa-ohana">
                 <h3>Pesquisa Ohana</h3>
-                <p>Nosso projeto tem como principal objetivo entender as dificuldades das instituições e, para isso, estamos fazendo essa pesquisa a fim de entender quais as maiores necessidades das ONGs e OSCIPs a fim de construir um material que seja útil, prático e de linguagem acessível, democratizando o conhecimento.</p>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfKzdW33FL0lYTOpVrwlPhFBrbednN2q067EiPHtDYBIocyBg/viewform" target="_blank">Acesse nossa pesquisa</a>    
+                <p>Nosso projeto tem como principal objetivo entender as dificuldades das instituições e, para isso,
+                    estamos fazendo essa pesquisa a fim de entender quais as maiores necessidades das ONGs e OSCIPs a
+                    fim de construir um material que seja útil, prático e de linguagem acessível, democratizando o
+                    conhecimento.</p>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfKzdW33FL0lYTOpVrwlPhFBrbednN2q067EiPHtDYBIocyBg/viewform"
+                    target="_blank">Acesse nossa pesquisa</a>
             </div>
         </div>
-
         <div class="index-container-impactos">
             <div class="index-impactos-ilustra">
                 <img src="./assets/impactos-ilustracao.svg" alt="Ilustração da seção de impactos">
             </div>
             <div class="index-impactos-text">
                 <h2>Impactos</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat.</p>
+                <p>Já trabalhamos com 4 instituições, mas pretendemos retomar o contato para dar prosseguimento ao
+                    Ciclo de formação, com uma abordagem mais próxima. Ademais com a recente reformulação do projeto,
+                    lançamos uma pesquisa de interesse para entender as principais dores das ONGs e OSCIPs e como poderíamos
+                    ajudá-las através das nossas frentes (ciclo de formações e aplicativos) e obtivemos 28 respostas dos mais
+                    variados tipos de organizações (principalmente as menores) com diversos tipos de dores e solicitando ajuda 
+                    e é baseado nisso que estamos montando todo nosso conteúdo, para ajudá-las no que mais precisam. Com o 
+                    surgimento do OhanaCast estamos lançando episódios quinzenalmente, ao fim de cada episódio divulgamos
+                    uma instituição diferente e com isso conseguirmos dar maior visibilidade a essas instituições.
+                </p>
             </div>
-        </div>
 
-        <div class="index-container-parcerias">
+        </div>
+        <!-- <div class="index-container-parcerias">
             <div class="index-parcerias-text">
                 <h2>Parcerias</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
@@ -74,8 +82,9 @@
                     <h3>Parceiro 2</h3>
                 </div>
             </div>
-        </div>
-</main>
+        </div> -->
+    </main>
+
 
 <!-- footer -->
 <?php  include 'footer.html'; ?>
