8b20e3c108ca341afb27867b73ca1413dc419b53
Lais Dias <laisdias099@gmail.com>
2020-11-28 23:32:51 -0300

    cria novo header responsivo sem bootstrap e exclui arquivos nao utilizados

---
+++ js/responsive-header.js
@@ -0,0 +1,18 @@
+$(document).ready(
+    function () {
+        $('.nav-hamburguer').click(function () {
+            // $('.nav-dropdown').css('display', 'none');
+            $('.nav-menu').slideToggle();
+        });
+
+        $('.item-dropdown').click(function() {
+            $('.nav-dropdown').slideToggle();
+        });
+                
+        $(window).resize(function () {
+            if ($(window).width() > 992) {
+                $('ul').removeAttr('style')
+            }
+        });
+    }
+);
