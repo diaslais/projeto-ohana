6322e5ba63f52fd9e4b281a6077ca72d79d64094
Lais Dias <laisdias099@gmail.com>
2020-12-06 16:56:06 -0300

    testa funcionamento do envio de formulario

--- js/formAlert.js
+++ js/formAlert.js
@@ -1,13 +1,20 @@
-document.querySelector("form").addEventListener("submit", handleSubmit);
+let testForm = document.querySelector("#contact form");
 
-const handleSubmit = (e) => {
-  e.preventDefault()
-  let myForm = document.getElementById('form-id');
-  let formData = new FormData(myForm)
-  fetch('/', {
-    method: 'POST',
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString()
-  }).then(() => console.log('Form successfully submitted')).catch((error) =>
-    alert(error))
-}
+testForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const formData = new FormData(testForm);
+    fetch(testForm.getAttribute('action'), {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Content Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        body: new URLSearchParams(formData).toString()
+    })
+    .then(res => {
+        if(res) {
+            alert("Obrigado por enviar sua mensagem!");
+        }
+    });
+});
