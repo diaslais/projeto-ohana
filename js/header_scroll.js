064f88a6b4ed82d3ee024bceff05189d3e52b551
Lais Dias <laisdias099@gmail.com>
2020-12-18 15:17:45 -0300

    remove efeito do header

--- js/header_scroll.js
+++ js/header_scroll.js
@@ -1,18 +1,14 @@
-$(document).ready(
-    function() {
-        var prevY = window.pageYOffset;
-        window.onscroll = function () {
-            if (window.outerWidth < 768){
-                return false;
-            }
-            var currentY = window.pageYOffset;
-            if (currentY < prevY) {
-                document.querySelector("header").style.top = "0";
-            }
-            else {
-                document.querySelector("header").style.top = "-100px";
-            }
-            prevY = currentY;
-        }
-    }    
-);
+var prevY = window.pageYOffset;
+window.onscroll = function () {
+    if (window.outerWidth < 768){
+        return false;
+    }
+    var currentY = window.pageYOffset;
+    if (currentY < prevY) {
+        document.querySelector("header").style.top = "0";
+    }
+    else {
+        document.querySelector("header").style.top = "-100px";
+    }
+    prevY = currentY;
+}
